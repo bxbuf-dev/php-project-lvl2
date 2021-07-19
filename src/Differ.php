1585c12e838948725ebf09ca5ac5ee69b0a832e9
Aleksei <bxbuf-dev@outlook.com>
2021-07-19 11:36:42 +0200

    var names changed to better code understatding

--- src/Differ.php
+++ src/Differ.php
@@ -4,29 +4,27 @@ namespace Differ\Differ;
 
 function genDiff(string $filePath1, string $filePath2)
 {
-    $file1 = file_get_contents($filePath1);
-    $file2 = file_get_contents($filePath2);
-    $fileData1 = json_decode($file1, true);
-    $fileData2 = json_decode($file2, true);
+    $file1 = json_decode(file_get_contents($filePath1), true);
+    $file2 = json_decode(file_get_contents($filePath2), true);
 
-    $existInFirst = array_diff($fileData1, $fileData2);
-    $existInSecond = array_diff($fileData2, $fileData1);
-    $sameInBoth = array_diff($fileData1, $existInFirst);
+    $file1Diff = array_diff($file1, $file2);
+    $file2Diff = array_diff($file2, $file1);
+    $noDiff = array_diff($file1, $file1Diff);
 
-    $allKeys = array_keys(array_merge($fileData1, $fileData2));
+    $allKeys = array_keys(array_merge($file1, $file2));
     asort($allKeys, SORT_STRING);
     $allKeys = array_values($allKeys);
 
     $result = "";
     foreach ($allKeys as $key) {
-        if (array_key_exists($key, $sameInBoth)) {
-            $result = $result . "  {$key}: " . json_encode($sameInBoth[$key]) . PHP_EOL;
+        if (array_key_exists($key, $noDiff)) {
+            $result = $result . "  {$key}: " . json_encode($noDiff[$key]) . PHP_EOL;
         }
-        if (array_key_exists($key, $existInFirst)) {
-            $result = $result . "- {$key}: " . json_encode($existInFirst[$key]) . PHP_EOL;
+        if (array_key_exists($key, $file1Diff)) {
+            $result = $result . "- {$key}: " . json_encode($file1Diff[$key]) . PHP_EOL;
         }
-        if (array_key_exists($key, $existInSecond)) {
-            $result = $result . "+ {$key}: " . json_encode($existInSecond[$key]) . PHP_EOL;
+        if (array_key_exists($key, $file2Diff)) {
+            $result = $result . "+ {$key}: " . json_encode($file2Diff[$key]) . PHP_EOL;
         }
     }
     return $result;
