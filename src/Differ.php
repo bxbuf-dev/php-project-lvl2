8f74d328a1d967582e271b66135922f8308605ad
Aleksei <bxbuf-dev@outlook.com>
2021-07-21 18:46:45 +0200

    created key names in array contained differences

--- src/Differ.php
+++ src/Differ.php
@@ -7,7 +7,7 @@ function genDiff(string $filePath1, string $filePath2)
     $data1 = getDataFromFile($filePath1, true);
     $data2 = getDataFromFile($filePath2, true);
     $result = getDifference($data1, $data2);
-    return convertToString($result, 'str');
+    return convertToString($result);
 }
 
 function getDataFromFile(string $filePath, bool $isJson): array
@@ -28,26 +28,36 @@ function getDifference(array $data1, array $data2): array
     $result = [];
     foreach ($allKeys as $key) {
         if (array_key_exists($key, $noDiff)) {
-            $result[] = [" ", $key, $noDiff[$key]];
+            $result[] = [
+                'stat' => " ",
+                'name' => $key,
+                'value' => $noDiff[$key]
+            ];
         }
         if (array_key_exists($key, $data1Diff)) {
-            $result[] = ["-", $key, $data1Diff[$key]];
+            $result[] = [
+                'stat' => "-",
+                'name' => $key,
+                'value' => $data1Diff[$key]
+            ];
         }
         if (array_key_exists($key, $data2Diff)) {
-            $result[] = ["+", $key, $data2Diff[$key]];
+            $result[] = [
+                'stat' => "+",
+                'name' => $key,
+                'value' => $data2Diff[$key]
+            ];
         }
     }
 
     return $result;
 }
 
-function convertToString(array $data, string $format): string
+function convertToString(array $data): string
 {
     $result = [];
-    if ($format == 'str') {
-        foreach ($data as $key) {
-            $result[] = "{$key[0]} {$key[1]}: " . json_encode($key[2]);
-        }
+    foreach ($data as $key) {
+        $result[] = "{$key['stat']} {$key['name']}: " . json_encode($key['value']);
     }
     return implode(PHP_EOL, $result) . PHP_EOL;
 }
